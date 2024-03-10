3600d2ee4c55132bc9df4690b15e6780a1bd82cc
Austin Shih <austinhshih@gmail.com>
2024-03-09 18:35:24 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,5 +16,30 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+      strapi.db.lifecycles.subscribe(async (event) => {
+        const listen_models = [
+            "events", 
+            "announcements", 
+            "current-members", 
+            "homepage", 
+            "members", 
+            "newsletters", 
+            "events-regional-state"
+        ]
+
+        const listen_actions = [
+            "afterUpdate",
+            "afterDelete",
+        ]
+
+        const model = event.model.collectionName;
+        const action = event.action;
+
+        if (listen_models.includes(model) && listen_actions.includes(action)) {
+            await fetch(`https://staging.ucdbestbuddies.org/api/revalidate`)
+            await fetch(`https://ucdbestbuddies.org/api/revalidate`)
+        }
+      });
+    },
 };
